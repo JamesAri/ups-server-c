88a9355acce21bc10457776c50907d453d2be1f5
Jakub Slechta <jakub.slechta@emplifi.io>
2022-10-09 15:32:23 +0200

    hehe

--- temp/client.c
+++ temp/client.c
@@ -1,8 +1,11 @@
 #include "client.h"
+#include "../utils/serialization.h"
 #include "../utils/sock_header.h"
+#include "../utils/sock_utils.h"
+#include "../utils/debug.h"
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <netdb.h>
@@ -11,39 +14,28 @@
 
 #include <arpa/inet.h>
 
-void print_buf_to_hex(void *buffer, int len) {
-    int i;
-    for (i = 0; i < len; i++) {
-        if (i > 0) printf(":");
-        printf("%02X", ((char *) buffer)[i]);
-    }
-    printf("\n");
+
+int recv_buffer(int fd, struct Buffer *buffer, int size) {
+    int res_recv, temp = size;
+    reserve_space(buffer, size);
+    res_recv = recvall(fd, buffer->data + buffer->next, &temp);
+    buffer->next += size;
+    fprintf(stdout, "recieved data: ");
+    print_buf_to_hex(buffer->data, buffer->next);
+    return res_recv;
 }
 
-
-struct Buffer *new_buffer() {
-    struct Buffer *b = malloc(sizeof(struct Buffer));
-
-    b->data = malloc(10);
-    b->size = 10;
-    b->next = 0;
-
-    return b;
+int recv_buffer_sock_header(int fd, struct Buffer *buffer) {
+    return recv_buffer(fd, buffer, sizeof(struct SocketHeader));
 }
 
-void reserve_space(struct Buffer *b, int bytes) {
-    if ((b->next + bytes) > b->size) {
-        b->data = realloc(b->data, b->size * 2);
-        b->size *= 2;
-    }
+int recv_buffer_int(int fd, struct Buffer *buffer) {
+    return recv_buffer(fd, buffer, sizeof(int));
 }
 
-void free_buffer(struct Buffer **buffer) {
-    free((*buffer)->data);
-    free((*buffer));
-    (*buffer) = NULL;
+int recv_buffer_string(int fd, struct Buffer *buffer, int str_len) {
+    return recv_buffer(fd, buffer, str_len);
 }
-
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr_temp(struct sockaddr *sa) {
@@ -54,9 +46,13 @@ void *get_in_addr_temp(struct sockaddr *sa) {
     return &(((struct sockaddr_in6 *) sa)->sin6_addr);
 }
 
+int send_buffer(int dest_fd, struct Buffer *buffer) {
+    int temp = buffer->next;
+    return sendall(dest_fd, buffer->data, &temp);
+}
+
 int main(int argc, char *argv[]) {
-    int server_socket, numbytes;
-    char buf[WORD_BUF_SIZE];
+    int server_socket;
     struct addrinfo hints, *servinfo, *p;
     int rv;
     char s[INET6_ADDRSTRLEN];
@@ -98,81 +94,90 @@ int main(int argc, char *argv[]) {
 
     freeaddrinfo(servinfo);
 
-    struct SocketHeader *socket_header = malloc(sizeof(struct SocketHeader));
-    struct Buffer *buffer = new_buffer();
-    int temp;
+    // send username
+    char *username = argv[1];
+    struct Buffer *out_buffer = new_buffer(), *in_buffer = new_buffer();
+    serialize_sock_header(INPUT_USERNAME, out_buffer);
+    serialize_int(strlen(username), out_buffer);
+    serialize_string(username, out_buffer);
 
-    temp = sizeof(struct SocketHeader);
-    reserve_space(buffer, temp);
-    socket_header->flag = INPUT_USERNAME;
-    memcpy(buffer->data, socket_header, temp);
-    buffer->next += temp;
+    send_buffer(server_socket, out_buffer);
+    print_buf_to_hex(out_buffer->data, out_buffer->next);
+    clear_buffer(out_buffer);
+    fprintf(stderr, "<username: %s>\n", username);
 
-    temp = sizeof(int);
-    reserve_space(buffer, temp);
-    int htonl_str_len = htonl(strlen(argv[1]));
-    memcpy(buffer->data + buffer->next, &htonl_str_len, temp);
-    buffer->next += temp;
+    int server_msg_len;
+    int int_offset = sizeof(struct SocketHeader);
+    int string_offset = int_offset + sizeof(int);
 
-    temp = strlen(argv[1]);
-    reserve_space(buffer, temp);
-    memcpy(buffer->data + buffer->next, argv[1], temp);
-    buffer->next += temp;
-
-    print_buf_to_hex(buffer->data, buffer->next);
-
-    // send username...
-    send(server_socket, buffer->data, buffer->next, 0);
-
-    memset(buffer->data, 0, buffer->size);
-    buffer->next = 0;
-
-    reserve_space(buffer, sizeof(struct SocketHeader));
-    socket_header->flag = CHAT;
-    memcpy(buffer->data, socket_header, sizeof(struct SocketHeader));
-    buffer->size += sizeof(struct SocketHeader);
-
-    char send_buffer[WORD_BUF_SIZE], recv_buffer[WORD_BUF_SIZE];
-    int cpid = fork();
-    if (cpid == 0) {
-        // child
+    char chat_buffer[WORD_BUF_SIZE], server_response_buf[WORD_BUF_SIZE];
+    int pid = fork();
+    if (pid == 0) {
         while (1) {
-            bzero(&send_buffer, sizeof(send_buffer));
-            fgets(send_buffer, 10000, stdin);
-            int str_len = strlen(send_buffer);
-            int htonl_str_len = htonl(str_len);
+            clear_buffer(out_buffer);
+            memset(chat_buffer, 0, sizeof(chat_buffer));
 
-            reserve_space(buffer, sizeof(int));
-            memcpy(buffer->data + buffer->size, &htonl_str_len, sizeof(int));
-            buffer->size += sizeof(int);
+            fgets(chat_buffer, 1000, stdin);
+            chat_buffer[strcspn(chat_buffer, "\r\n")] = 0;
 
-            reserve_space(buffer, str_len);
-            memcpy(buffer->data + buffer->size, send_buffer, str_len);
-            buffer->size += str_len;
-
-            send(server_socket, buffer->data, buffer->next, 0);
-            memset(buffer->data, 0, buffer->size);
-            buffer->next = 0;
+            serialize_sock_header(CHAT, out_buffer);
+            serialize_int(strlen(chat_buffer), out_buffer);
+            serialize_string(chat_buffer, out_buffer);
+            int send_res = send_buffer(server_socket, out_buffer);
+            if (send_res <= 0) {
+                fprintf(stderr, "error, couldn't send data\n");
+                exit(1);
+            }
         }
     } else {
-        // parent
         while (1) {
-            bzero(&recv_buffer, sizeof(recv_buffer));
-            recv(server_socket, recv_buffer, sizeof(recv_buffer), 0);
-            printf("SERVER : %s", recv_buffer);
-        }
-    }
+            clear_buffer(in_buffer);
+            memset(server_response_buf, 0, sizeof(server_response_buf));
 
-    char username[WORD_BUF_SIZE] = "Bueno";
-    send(server_socket, username, WORD_BUF_SIZE, 0);
-    unsigned char s_header_flag;
-    recv(server_socket, &s_header_flag, sizeof(s_header_flag), 0);
-    if (s_header_flag == INVALID_USERNAME) {
-        fprintf(stderr, "Invalid username! Terminating...");
-        exit(1);
-    } else {
-        recv(server_socket, buf, sizeof(buf), 0);
-        printf("%s", buf);
+            reserve_space(in_buffer, sizeof(struct SocketHeader));
+            int recv_res;
+            if ((recv_res = recv(server_socket, in_buffer->data, sizeof(struct SocketHeader), 0)) == 0) {
+                free_buffer(&in_buffer);
+                fprintf(stderr, "lost server connection - server down or not responding\n");
+                exit(1);
+            } else if (recv_res < 0) {
+                perror("ERRNO(received < 0 value): ");
+            }
+            in_buffer->next += sizeof(struct SocketHeader);
+
+            int recv_flag = ((struct SocketHeader *) in_buffer->data)->flag;
+
+            switch (recv_flag) {
+                case CHAT:
+                    fprintf(stdout, "received CHAT flag\n");
+                    recv_buffer_int(server_socket, in_buffer);
+                    server_msg_len = ntohl(*(int *) (in_buffer->data + int_offset));
+                    recv_buffer_string(server_socket, in_buffer, server_msg_len);
+                    fprintf(stdout, "%s\n", (char *) (in_buffer->data + string_offset));
+                    break;
+                case CANVAS:
+                    fprintf(stdout, "received CANVAS flag\n");
+                    break;
+                case WRONG_GUESS:
+                    fprintf(stdout, "received WRONG_GUESS flag\n");
+                    break;
+                case CORRECT_GUESS:
+                    fprintf(stdout, "received CORRECT_GUESS flag\n");
+                    break;
+                case INVALID_USERNAME:
+                    fprintf(stdout, "received INVALID_USERNAME flag\n");
+                    break;
+                case CORRECT_GUESS_ANNOUNCEMENT:
+                    fprintf(stdout, "received CORRECT_GUESS_ANNOUNCEMENT flag\n");
+                    recv_buffer_int(server_socket, in_buffer);
+                    server_msg_len = ntohl(*(int *) (in_buffer->data + int_offset));
+                    recv_buffer_string(server_socket, in_buffer, server_msg_len);
+                    fprintf(stdout, "%s\n", (char *) (in_buffer->data + string_offset));
+                    break;
+                default:
+                    fprintf(stderr, "UNKNOWN SOCKET HEADER: received flag: %d\n", recv_flag);
+            }
+        }
     }
 
     close(server_socket);
--- temp/client.h
+++ temp/client.h
@@ -6,14 +6,6 @@
 #define UPS_SERVER_C_CLIENT_H
 
 #define PORT "9034"
-#define BACKLOG 5
 #define WORD_BUF_SIZE 256
-#define CANVAS_BUF_SIZE 1024
-
-struct Buffer {
-    void *data;
-    int next;
-    int size;
-};
 
 #endif //UPS_SERVER_C_CLIENT_H
